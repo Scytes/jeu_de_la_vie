53bfcf0b7574dedef65d72af998619f5261e0aba
Scytes <tanatosdu31@hotmail.fr>
2013-03-11 17:45:06 +0100

    Passage du projet en utf-8

--- utilitaire.cpp
+++ utilitaire.cpp
@@ -1,8 +1,8 @@
 #include "utilitaire.h"
 
-/*
+/**
     Retourne une Position dont l'abscisse et l'ordonnée sont < aux valeurs passées en paramètre.
-*/
+**/
 Position randomPos (const unsigned int _maxAbs, const unsigned int _maxOrd)
 {
     // On crée la nouvelle position
