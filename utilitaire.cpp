379226257993ebb992c73017cfa045c30e4eec57
Scytes <tanatosdu31@hotmail.fr>
2013-03-11 17:50:53 +0100

    Test cdoc

--- utilitaire.cpp
+++ utilitaire.cpp
@@ -1,11 +1,13 @@
 #include "utilitaire.h"
 
 /**
-    Retourne une Position dont l'abscisse et l'ordonnée sont < aux valeurs passées en paramètre.
+    Retourne une Position dont l'abscisse et l'ordonnÃ©e sont < aux valeurs passÃ©es en paramÃ¨tre.
+    @param _maxAbs La valeur de l'abscisse maximale.
+    @param _maxOrd La valeur de l'ordonnÃ©e maximale.
 **/
 Position randomPos (const unsigned int _maxAbs, const unsigned int _maxOrd)
 {
-    // On crée la nouvelle position
+    // On crÃ©e la nouvelle position
     Position newPos((int) rand()%(_maxAbs), (int) rand()%(_maxOrd)) ;
     return newPos ;
 }
--- utilitaire.h
+++ utilitaire.h
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include "Position.h"
 
-/*
-    Retourne une Position dont l'abscisse et l'ordonnée sont < aux valeurs passées en paramètre.
-*/
+/**
+    Retourne une Position dont l'abscisse et l'ordonnÃ©e sont < aux valeurs passÃ©es en paramÃ¨tre.
+    @param _maxAbs La valeur de l'abscisse maximale.
+    @param _maxOrd La valeur de l'ordonnÃ©e maximale.
+**/
 Position randomPos (const unsigned int, const unsigned int) ;
 
 #endif // UTILITAIRE_H
